5e6aa0f4b9a376c1dbffea01690c17ccc87c74c2
Nico Franzese <franzesens@gmail.com>
2017-10-14 01:37:48 -0300

    Se generan menus, y ruteos, se realizan conexiones con firebase para grabar las cargas y traer las mismas.

--- cargaCredito/src/app/app.module.ts
+++ cargaCredito/src/app/app.module.ts
@@ -3,38 +3,65 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+// import { AboutPage } from '../pages/about/about';
+// import { ContactPage } from '../pages/contact/contact';
+// import { HomePage } from '../pages/home/home';
+// import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
+//import { Observable } from 'rxjs/Observable';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CargaCreditoProvider } from '../providers/carga-credito/carga-credito';
+import { CargaCreditoPage } from '../pages/carga-credito/carga-credito';
+import { MenuPage } from '../pages/menu/menu';
+import { SaldoPage } from '../pages/saldo/saldo';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { CONFIG } from './firebase';
+
+//import { Http } from '@angular/http';
+import { HttpModule } from '@angular/http';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    // AboutPage,
+    // ContactPage,
+    // HomePage,
+    // TabsPage,
+    LoginPage,
+    CargaCreditoPage,
+    MenuPage,
+    SaldoPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(CONFIG),
+    AngularFireDatabaseModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    // AboutPage,
+    // ContactPage,
+    // HomePage,
+    // TabsPage,
+    LoginPage,
+    CargaCreditoPage,
+    MenuPage,
+    SaldoPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    BarcodeScanner,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CargaCreditoProvider
   ]
 })
 export class AppModule {}
